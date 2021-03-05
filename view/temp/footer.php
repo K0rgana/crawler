49b4e1c32609ed3e8c23885e2b81b33c24ef6104
alowce <alicebrandt.c@gmail.com>
2021-03-05 01:06:05 -0300

    Ajustando footer

--- view/temp/footer.php
+++ view/temp/footer.php
@@ -1,35 +1,22 @@
 <footer class="mt-2" style="background-color: #9ACD32">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md text-center">
+      <div class="col-md-2"></div>
+      <div class="col-md-4 text-center">
         <a class="navbar-brand" href="/">
-          <img src="img/logoCrawler2.png" style="width:120px; margin-top: 70%">
+          <img src="img/logoCrawler2.png" style="width:120px; margin-top: 18px">
         </a>
-        <p class="text-white font-weight-bold">© 2020 Crawler - Todos os direitos reservados.</p>
+        <p class="text-white font-weight-bold pr-3">© 2020 Crawler - Todos os direitos reservados.</p>
       </div>
-    <div class="col-md"></div>
-    <div class="col-md mt-1">
-      <ul class="navbar-nav text-center">
-        <li class="nav-item">
-          <a class="nav-link text-white font-weight-bold" href="#">Sobre</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white font-weight-bold" href="#">Parceiros</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white font-weight-bold" href="#">Link1</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white font-weight-bold" href="#">Link2</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white font-weight-bold" href="#">Link3</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link text-white font-weight-bold" href="#">Link4</a>
-        </li>
-      </ul>
+    <div class="col-md-4 text-left" style="margin-top: 47px">
+      <a class="text-white font-weight-bold pr-3" href="#">Sobre</a>
+      <a class="text-white font-weight-bold pr-3" href="#">Parceiros</a>
+      <a class="text-white font-weight-bold pr-3" href="#">Link1</a>
+      <a class="text-white font-weight-bold pr-3" href="#">Link2</a>
+      <a class="text-white font-weight-bold pr-3" href="#">Link3</a>
+      <a class="text-white font-weight-bold pr-3" href="#">Link4</a>
     </div>
+    <div class="col-md-2"></div>
     </div>
   </div>
 </footer>
