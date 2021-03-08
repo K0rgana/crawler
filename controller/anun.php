326975c8a486282f9fc19dc758f7c12e2b6395e7
K0rgana <anagrom1999@gmail.com>
2021-03-08 15:18:14 -0300

    adiciona consulta por endereço de anuncio

--- controller/anun.php
+++ controller/anun.php
@@ -11,5 +11,10 @@ $stmt = $pdo->prepare('SELECT * FROM cwl_usuario WHERE usu_id = ?');
 $stmt->execute([$anun[0]['anu_usu_id']]);
 
 $usu = $stmt->fetchAll();
+
+$stmt = $pdo->prepare('SELECT * FROM cwl_endereco WHERE end_id = ?');
+$stmt->execute([$anun[0]['anu_end_id']]);
+
+$end = $stmt->fetchAll();
                 
  ?>
