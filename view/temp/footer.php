2517a13f493c2a94234f9191dcd515e1dd7b2976
alowce <alicebrandt.c@gmail.com>
2021-03-08 22:12:02 -0300

    Modificando footer

--- view/temp/footer.php
+++ view/temp/footer.php
@@ -1,22 +1,18 @@
 <footer class="mt-2" style="background-color: #9ACD32">
   <div class="container-fluid">
     <div class="row">
-      <div class="col-md-2"></div>
-      <div class="col-md-4 text-center">
+      <div class="col-md-1"></div>
+      <div class="col-md-5 ml-n5 text-center">
         <a class="navbar-brand" href="/">
           <img src="img/logoCrawler2.png" style="width:120px; margin-top: 18px">
         </a>
         <p class="text-white font-weight-bold pr-3">© 2020 Crawler - Todos os direitos reservados.</p>
       </div>
-    <div class="col-md-4 text-left" style="margin-top: 47px">
+    <div class="col-md-5 text-right" style="margin-top: 47px">
       <a class="text-white font-weight-bold pr-3" href="#">Sobre</a>
       <a class="text-white font-weight-bold pr-3" href="#">Parceiros</a>
-      <a class="text-white font-weight-bold pr-3" href="#">Link1</a>
-      <a class="text-white font-weight-bold pr-3" href="#">Link2</a>
-      <a class="text-white font-weight-bold pr-3" href="#">Link3</a>
-      <a class="text-white font-weight-bold pr-3" href="#">Link4</a>
     </div>
-    <div class="col-md-2"></div>
+    <div class="col-md-1"></div>
     </div>
   </div>
 </footer>
